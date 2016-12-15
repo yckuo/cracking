6e80ad5b6bc409aa5f6b120106cef0f80fabad84
U-DESKTOP-L3TJHER\yckuo <kokokoko@gmail.com>
2016-12-14 20:49:39 -0800

    CCI 2.2 ReturnKthToLast Recursive version

--- ch2/2.2.cpp
+++ ch2/2.2.cpp
@@ -5,6 +5,8 @@
 #include "ListNode.h"
 using namespace std;
 
+typedef pair<ListNode*, int> P;
+
 class Solution {
 public:
     ListNode* KthToLast(ListNode* head, int k) {
@@ -21,6 +23,16 @@ public:
         }
         return slow;
     }
+
+    // Recursive solution
+    P KthToLast2(ListNode* head, int k) {
+        if (!head) return {NULL, 0};
+        P p = KthToLast2(head->next, k);
+        int count = 1 + p.second;
+        if (p.first) return {p.first, count};
+        if (count == k) return {head, count};
+        return {NULL, count};
+    }
 };
 
 int main(int argc, char** argv) {
@@ -33,7 +45,9 @@ int main(int argc, char** argv) {
     Solution sol;
     int k;
     while (cin >> k) {
-        ListNode *node = sol.KthToLast(head, k);
+        // ListNode *node = sol.KthToLast(head, k);
+        P p = sol.KthToLast2(head, k);
+        ListNode *node = p.first;
         cout << k << ": ";
         if (!node) cout << "NULL" << endl;
         else cout << node->val << endl;
